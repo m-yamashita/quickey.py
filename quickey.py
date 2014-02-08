f23283236e21e9bb4a047b8b8fc2e6e4a9e11391
m-yamashita <mxyamashita@gmail.com>
2014-02-09 03:24:43 +0900

    ・Windowのアクティブ化をする際のwmctrlへの指定の仕方をタイトルではなく Window ID で渡すよう修正。これにより全く同じウィンドウタイトルが同時に存在した場合の重複問題が解決。

--- quickey.py
+++ quickey.py
@@ -5,15 +5,15 @@ import re
 # Main routine.
 def main():
     args = get_args();
-    titles = get_matched_window_titles(args.regexp, args.is_class)
+    window_ids = get_matched_window_ids(args.regexp, args.is_class)
     is_all = args.is_all
     is_reverse = args.is_reverse
-    if (is_all and not is_reverse) or (not is_all and is_reverse): titles.reverse()
-    if len(titles) <= 0:
+    if (is_all and not is_reverse) or (not is_all and is_reverse): window_ids.reverse()
+    if len(window_ids) <= 0:
         exec_command(args.command)
         return
-    for title in titles:
-        exec_command('wmctrl -a "%s"' % title.replace('"', '\\"'))
+    for window_id in window_ids:
+        exec_command('wmctrl -ia "%s"' % window_id)
         if not is_all: return
 
 # Get all arguments.
@@ -36,19 +36,19 @@ def get_args():
             help="Reverse the order of activation of windows.")
     return parser.parse_args()
 
-# Get window titles
-def get_matched_window_titles(target_regexp, is_class):
+# Get window id list.
+def get_matched_window_ids(target_regexp, is_class):
     regexp_pattern = re.compile(target_regexp)
     wmctrl_option = "lx" if is_class else "l"
     wmctrl_list = exec_command("wmctrl -%s" % wmctrl_option).split('\n')
-    titles = []
+    ids = []
     for row in wmctrl_list:
         columns = split_columns_by_wmctrl_row(row)
         title = get_title_by_columns(columns, is_class)
         search_target = get_wm_class_by_columns(columns) if is_class else title
         if not regexp_pattern.search(search_target): continue
-        titles.append(title)
-    return titles;
+        ids.append(columns[0])
+    return ids;
 
 # Split to column by the row of "wmctrl -l[x]" command result.
 def split_columns_by_wmctrl_row(wmctrl_list_row):
