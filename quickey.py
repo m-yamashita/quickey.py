ae029f3e44ab746e22e36d79460f31d30896b6f7
m-yamashita <mxyamashita@gmail.com>
2014-02-09 02:31:08 +0900

    ・検索条件に複数件マッチしてしまうと動作しなくなるバグを仕様ごと修正。 ・コマンドラインオプションを追加。   ・-c オプションで、WM_CLASS名から検索することが可能になった。   ・-a オプションで、検索により複数のウィンドウがマッチした場合に、全てのウィンドウを順にアクティブ化できるようになった。   ・-r オプションで、検索によりマッチしたウィンドウをアクティブ化する順序を逆にすることができるようになった。-aオプションと合わせる他、-aオプションがついていない場合にも、新しいプロセスのものを優先的に利用するように変更したりする事が可能になった。 ・awkを利用せずPythonの処理のみを利用するよう修正。 ・大幅なリファクタリング及びmain関数の実装等、ちょっとPythonらしいコードになった。

--- quickey.py
+++ quickey.py
@@ -1,17 +1,72 @@
+import argparse
 import subprocess
 import re
-import sys
 
 # Main routine.
-def go(target_string, path):
-    wmctrl_list = exec_command("wmctrl -l").split("\n")
+def main():
+    args = get_args();
+    titles = get_matched_window_titles(args.regexp, args.is_class)
+    is_all = args.is_all
+    is_reverse = args.is_reverse
+    if (is_all and not is_reverse) or (not is_all and is_reverse): titles.reverse()
+    if len(titles) <= 0:
+        exec_command(args.command)
+        return
+    for title in titles:
+        exec_command('wmctrl -a "%s"' % title.replace('"', '\\"'))
+        if not is_all: return
 
-    pattern = re.compile(target_string)
-    if any(pattern.search(line) for line in wmctrl_list):
-        title = exec_command("wmctrl -l | awk '/%s/ {for(i=4;i<NF;++i){printf(\"%%s \",$i)}print $NF}'" % target_string)
-        exec_command("wmctrl -a \"%s\"" % title)
-    else:
-        exec_command(path)
+# Get all arguments.
+def get_args():
+    parser = argparse.ArgumentParser(description="quickey.py is wmctrl's wrapper script.")
+    parser.add_argument(
+            'regexp',
+            help="Regexp for search from all window titles(if '-c' option used, it's WM_CLASS).")
+    parser.add_argument(
+            'command',
+            help="Run this command if nothing matches in window titles(or WM_CLASS) by 'regexp' argument.")
+    parser.add_argument(
+            '-c', action="store_true", dest="is_class", default=False,
+            help="Change regexp target to WM_CLASS from title.")
+    parser.add_argument(
+            '-a', action="store_true", dest="is_all", default=False,
+            help="Activate all windows in matched regexp.")
+    parser.add_argument(
+            '-r', action="store_true", dest="is_reverse", default=False,
+            help="Reverse the order of activation of windows.")
+    return parser.parse_args()
+
+# Get window titles
+def get_matched_window_titles(target_regexp, is_class):
+    regexp_pattern = re.compile(target_regexp)
+    wmctrl_option = "lx" if is_class else "l"
+    wmctrl_list = exec_command("wmctrl -%s" % wmctrl_option).split('\n')
+    titles = []
+    for row in wmctrl_list:
+        columns = split_columns_by_wmctrl_row(row)
+        title = get_title_by_columns(columns, is_class)
+        search_target = get_wm_class_by_columns(columns) if is_class else title
+        if not regexp_pattern.search(search_target): continue
+        titles.append(title)
+    return titles;
+
+# Split to column by the row of "wmctrl -l[x]" command result.
+def split_columns_by_wmctrl_row(wmctrl_list_row):
+    columns = wmctrl_list_row.split(" ")
+    while "" in columns: columns.remove("")
+    return columns
+
+
+# Get window title by splitted columns info.
+def get_title_by_columns(columns, is_class):
+    title_column_index = 5 if is_class else 4
+    title = " ".join(columns[title_column_index - 1:])
+    return title
+
+# If '-c' option used, get WM_CLASS's string.
+def get_wm_class_by_columns(columns):
+    target_column_index = 2
+    return columns[target_column_index]
 
 # Execute system command by argument.
 def exec_command(command):
@@ -23,4 +78,5 @@ def exec_command(command):
             ).stdout.read()[:-1]
 
 # Call the main routine.
-go(sys.argv[1], sys.argv[2])
+if __name__ == "__main__":
+    main()
